96daa6fcfb6cedf479cac2e5fd83017f93fb3bc1
abdulrahmanmahmood <abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2024-02-19 14:48:01 +0200

    editing localstrage error

--- app/_RTK/StoreProvider.js
+++ app/_RTK/StoreProvider.js
@@ -1,32 +1,7 @@
-'use client'
-// import { useRef } from 'react'
-import { Provider } from 'react-redux'
-import { store } from './store'
+"use client";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function StoreProvider({ children }) {
-  // const storeRef = useRef()
-  // if (!storeRef.current) {
-  //   // Create the store instance the first time this renders
-  //   storeRef.current = makeStore()
-  // }
-
-  return <Provider store={store}>{children}</Provider>
+  return <Provider store={store}>{children}</Provider>;
 }
-
-
-
-// 'use client'
-// import { useRef } from 'react'
-// import { Provider } from 'react-redux'
-// import { makeStore } from './store'
-// import { initializeCount } from './slices/coutnerSlice'
-
-// export default function StoreProvider({ count, children }) {
-//   const storeRef = useRef(null)
-//   if (!storeRef.current) {
-//     storeRef.current = makeStore()
-//     storeRef.current.dispatch(initializeCount(count))
-//   }
-
-//   return <Provider store={storeRef.current}>{children}</Provider>
-// }
--- app/_RTK/store.js
+++ app/_RTK/store.js
@@ -1,27 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import tokenReducer from './slices/tokenSlice';
-// import tokenSlice from './slices/tokenSlice';
-import visitorReducer from './slices/visitorSlice'
-
+import { configureStore } from "@reduxjs/toolkit";
+import tokenReducer from "./slices/tokenSlice";
+import visitorReducer from "./slices/visitorSlice";
 
 export const store = configureStore({
-    reducer: {
-      token:tokenReducer,
-      // counter:couterReducer,
-      visitor:visitorReducer
-    },
-  })
-
-
-
-// import { configureStore } from '@reduxjs/toolkit'
-// import counterReducer from './slices/coutnerSlice'
-
-// export const makeStore = () => {
-//   return configureStore({
-//     reducer: {
-
-//       counter:counterReducer
-//     },
-//   })
-// }
+  reducer: {
+    token: tokenReducer,
+    // counter:couterReducer,
+    visitor: visitorReducer,
+  },
+});
